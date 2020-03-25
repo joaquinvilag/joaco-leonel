d291a39dfc89dbd8c1a12fb029d8a740662583a2
joaquinvila.g <joaquinvila@gmail.com>
2020-03-25 16:30:29 -0300

    prueba final

--- Leonel-1.js
+++ Leonel-1.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-var Numero = 2;
-var saludo = "Hola, que tal?";
+var numero = 2
+var saludo = "Hola, todo bien?"
+var marcaCelular = Xiaomi
 
 function multiplicar (factor) {
     return numero*factor;
 };
-=======
-var numero = 2
-var saludo = "Hola, todo bien?"
-var marcaCelular = Xiaomi
->>>>>>> 1007ead3fd7e46fd2be378975b10e4153c9df035
